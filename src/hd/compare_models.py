a46f25e30424b41a76e89fbcdd098bbc3f816378
matera02 <m.matera51@studenti.uniba.it>
2024-07-27 01:47:55 +0200

    aggiunta confronto modelli

--- src/hd/compare_models.py
+++ src/hd/compare_models.py
@@ -1,18 +1,26 @@
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
-
-def load_model_info(model_name):
-    with open(f"{model_name.replace(' ', '_').lower()}_model_info.json", "r") as f:
+import os
+def load_model_info(model_name, dir):
+    model_name = f"{model_name.replace(' ', '_').lower()}_model_info.json"
+    dest = os.path.join(dir, model_name)
+    with open(dest, "r") as f:
         return json.load(f)
 
-def compare_models():
-    models = ["Decision Tree", "Logistic Regression", "AdaBoost", "XGBoost", "ANN"]
+def compare_models(savefig='src/hd/data/results/comparison.png'):
+    models = [
+        ("Decision Tree", 'src/hd/data/results/dt/'), 
+        ("Logistic Regression", 'src/hd/data/results/lr/'), 
+        ("AdaBoost", 'src/hd/data/results/ab/'), 
+        ("XGBoost", 'src/hd/data/results/xgb/'), 
+        ("ANN", 'src/hd/data/results/ann/')
+    ]
     model_stats = []
     for model in models:
-        model_info = load_model_info(model)
+        model_info = load_model_info(model[0], model[1])
         model_stats.append({
-            "Model": model,
+            "Model": model[0],
             "Best Accuracy": model_info["best_accuracy"],
             "Final Accuracy": model_info["final_accuracy"],
             "Precision": model_info["precision"],
@@ -22,25 +30,26 @@ def compare_models():
     
     df = pd.DataFrame(model_stats)
     
-    # Create a figure and axis
+    # Creo figura e assi
     fig, ax = plt.subplots(figsize=(12, 6))
     
-    # Hide axes
+    # Nascondo asse
     ax.axis('off')
     
-    # Create the table
+    # Creo la tabella
     table = ax.table(cellText=df.values, colLabels=df.columns, cellLoc='center', loc='center')
     
-    # Set table properties
+    # Setto le proprietà della tabella
     table.auto_set_font_size(False)
     table.set_fontsize(10)
     table.scale(1.2, 1.5)
     
-    # Add a title
-    plt.title("Model Comparison", fontsize=16, pad=20)
+    # Titolo
+    plt.title("Confronto fra modelli", fontsize=16, pad=20)
     
-    # Show the plot
+    # Mostro il plot
     plt.tight_layout()
+    plt.savefig(savefig)
     plt.show()
 
 if __name__ == "__main__":
